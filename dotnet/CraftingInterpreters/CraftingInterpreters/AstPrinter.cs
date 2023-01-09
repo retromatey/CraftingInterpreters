3fb778a61950cf70e024e8e8be688a4dbcf773fc
Jason Tennant <jason.tennant@outlook.com>
2023-01-09 06:35:36 -0700

    Adding Chapter 05 code for dotnet (#3)

---
+++ dotnet/CraftingInterpreters/CraftingInterpreters/AstPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace CraftingInterpreters;
+
+public class AstPrinter : IVisitor<string>
+{
+    public string Print(Expr expr) => expr.Accept(this);
+    public string VisitBinaryExpr(Binary expr) => Parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right);
+    public string VisitGroupingExpr(Grouping expr) => Parenthesize("group", expr.Expression);
+    public string VisitLiteralExpr(Literal expr) => expr.Value == null ? "nil" : expr.Value.ToString();
+    public string VisitUnaryExpr(Unary expr) => Parenthesize(expr.Operator.Lexeme, expr.Right);
+    
+    private string Parenthesize(string name, params Expr[] exprs) {
+        var builder = new StringBuilder();
+
+        builder.Append('(').Append(name);
+
+        foreach (var expr in exprs) {
+            builder.Append(' ');
+            builder.Append(expr.Accept(this));
+        }
+
+        builder.Append(')');
+
+        return builder.ToString();
+    }
+}
---
+++ dotnet/CraftingInterpreters/CraftingInterpreters/Expr.cs
@@ -0,0 +1,33 @@
+﻿namespace CraftingInterpreters;
+
+public interface IVisitor<out T> {
+    T VisitBinaryExpr(Binary expr);
+    T VisitGroupingExpr(Grouping expr);
+    T VisitLiteralExpr(Literal expr);
+    T VisitUnaryExpr(Unary expr);
+}
+
+public abstract record Expr()
+{
+    public abstract T Accept<T>(IVisitor<T> visitor);
+}
+
+public record Binary(Expr Left, Token Operator, Expr Right) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor) => visitor.VisitBinaryExpr(this);
+}
+
+public record Grouping(Expr Expression) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor) => visitor.VisitGroupingExpr(this);
+}
+
+public record Literal(object Value) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor) => visitor.VisitLiteralExpr(this);
+}
+
+public record Unary(Token Operator, Expr Right) : Expr
+{
+    public override T Accept<T>(IVisitor<T> visitor) => visitor.VisitUnaryExpr(this);
+}
